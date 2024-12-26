d958f3bea3e7b026e446ffc8e4c1de2988fb0c37
Mertcan92 <mertcan55200555@gmail.com>
2024-12-26 16:20:05 +0300

    Ubung_1
    Mertcan_Ozturkmen_(26.12.2024)

---
+++ Mertcan(26.12.2024).py
@@ -0,0 +1,6 @@
+Jahre=int(input("Geben Sie Ihre Jahre ein"))
+Abschlussnote=int(input("Geben Sie Ihre Abschlussnote ein"))
+if 20<=Jahre<=50 and Abschlussnote>80 :
+  print("einstellen")
+else :
+  print("ablehnen")
